793313bdeea542db96e80d37b069ac30e5789ff2
Martin <92693481+meyiapir@users.noreply.github.com>
2024-03-16 10:51:18 +0300

    Update get-models-sd-official.sh

--- config/provisioning/get-models-sd-official.sh
+++ config/provisioning/get-models-sd-official.sh
@@ -17,31 +17,3 @@ if [[ ! -e ${model_file} ]]; then
     printf "Downloading Stable Diffusion 1.5...\n"
     wget -q -O ${model_file} ${model_url}
 fi
-
-# v2-1_768-ema-pruned
-model_file=${checkpoints_dir}/v2-1_768-ema-pruned.ckpt
-model_url=https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
-
-if [[ ! -e ${model_file} ]]; then
-    printf "Downloading Stable Diffusion 2.1...\n"
-    wget -q -O ${model_file} ${model_url}
-fi
-
-# sd_xl_base_1
-model_file=${checkpoints_dir}/sd_xl_base_1.0.safetensors
-model_url=https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-
-if [[ ! -e ${model_file} ]]; then
-    printf "Downloading Stable Diffusion XL base...\n"
-    wget -q -O ${model_file} ${model_url}
-fi
-
-# sd_xl_refiner_1
-model_file=${checkpoints_dir}/sd_xl_refiner_1.0.safetensors
-model_url=https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
-
-if [[ ! -e ${model_file} ]]; then
-    printf "Downloading Stable Diffusion XL refiner...\n"
-    wget -q -O ${model_file} ${model_url}
-fi
-
